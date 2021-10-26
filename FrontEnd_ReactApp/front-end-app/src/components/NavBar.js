00e4a14e35f03b513b6a5b26bca20014b2e8681b
TeishaMurray <teishamurray@gmail.com>
2021-10-26 19:47:41 -0400

    build: nav-bar setup complete

--- FrontEnd_ReactApp/front-end-app/src/components/NavBar.js
+++ FrontEnd_ReactApp/front-end-app/src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div >
-                Home | Beer List | Beer by Type | Add Beers | Food Pairing
-                <div className="navbar-list">
-                    
-                </div>
+            <div className="nav-bar">
+                <ul>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/allbeers">Check the List</Link></li>
+                    <li><Link to="/beer-by-type">Search by Type</Link></li>
+                </ul>
             </div>
         )
     }
